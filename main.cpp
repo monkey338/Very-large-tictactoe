4d19ed45b77ef7217bb979160c7b224f08fb3855
monkey338 <126100862+monkey338@users.noreply.github.com>
2023-08-09 18:55:20 -0400

    fixed bug when doing cross on a left

--- main.cpp
+++ main.cpp
@@ -190,6 +190,14 @@ int main() {
 	   cout << "You Win!\n";
 	   exit(0);
  }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
+ }
     check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
 	   cout << "You Win!\n";
@@ -250,6 +258,14 @@ if (o == "X" && o5 == "X" && o9 == "X") {
 	   cout << "You Win!\n";
 	   exit(0);
  }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
+ }
 
     check();
     if (o == "X" && o2 == "X" && o3 == "X") {
@@ -309,7 +325,15 @@ if (o == "X" && o5 == "X" && o9 == "X") {
 	   cout << "You Win!\n";
 	   exit(0);
  }
-  
+  if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ 
 check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
 	   cout << "You Win!\n";
@@ -366,6 +390,14 @@ if (o == "X" && o5 == "X" && o9 == "X") {
  if (o == "O" && o5 == "O" && o9 == "O") {
 	   cout << "You Win!\n";
 	   exit(0);
+ }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
  }
 
 check();
@@ -425,6 +457,14 @@ if (o == "X" && o5 == "X" && o9 == "X") {
 	   cout << "You Win!\n";
 	   exit(0);
  }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
+ }
 
 check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
@@ -482,6 +522,14 @@ if (o == "X" && o5 == "X" && o9 == "X") {
  if (o == "O" && o5 == "O" && o9 == "O") {
 	   cout << "You Win!\n";
 	   exit(0);
+ }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
  }
 
 check();
@@ -540,6 +588,14 @@ if (o == "X" && o5 == "X" && o9 == "X") {
 	   cout << "You Win!\n";
 	   exit(0);
  }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
+ }
 
 check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
@@ -597,6 +653,14 @@ if (o == "X" && o5 == "X" && o9 == "X") {
  if (o == "O" && o5 == "O" && o9 == "O") {
 	   cout << "You Win!\n";
 	   exit(0);
+ }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
  }
 
 check();
@@ -656,6 +720,15 @@ if (o == "X" && o5 == "X" && o9 == "X") {
 	   cout << "You Win!\n";
 	   exit(0);
  }
+ if (o3 == "X" && o5 == "X" && o7 == "X") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+ if (o3 == "O" && o5 == "O" && o7 == "O") {
+	cout << "You Win!\n";
+	exit(0);
+ }
+
  if (o == "X" or o == "O" && o2 == "X" or o2 == "O" && o3 == "X" or o3 == "O" && o4 == "X" or o4 == "O" && o5 == "X" or o5 == "O" && o6 == "X" or o6 == "O" && o7 == "X" or o7 == "O" && o8 == "X" or o8 == "O" && o9 == "X" or o9 == "O") {
     cout << "Draw!\n";
  }

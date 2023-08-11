37d07ad7343dcf901a5f0d92234c115fd3d01302
monkey338 <126100862+monkey338@users.noreply.github.com>
2023-08-11 19:34:54 -0400

    Add files via upload
    added names

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <cstdlib> // libraries
+#include <cstdlib>
 #include <iostream>
+#include <string>
 using namespace std;
 
-string o = " "; // variables
+string o = " ";
     string o2 = " ";
     string o3 = " ";
     string o4 = " ";
@@ -13,10 +14,12 @@ string o = " "; // variables
     string o9 = " ";
     int col;
     int row;
+    string name;
+    string name2;
 
 
-int draw() { // the function to draw the tictactoe like columns and rows
-cout << " _ _ _\n"; 
+int draw() {
+cout << " _ _ _\n";
     cout << "|";
     cout << o;
     cout << "|";
@@ -44,7 +47,7 @@ cout << " _ _ _\n";
 
 }
 
-int check() { // the function that checks that if user writes column and row with X
+int check() {
    cout << "Enter column: ";
     cin >> col;
     cout << "Enter row: ";
@@ -87,7 +90,7 @@ int check() { // the function that checks that if user writes column and row wit
     }
 }
 
-int check2() { // the function that checks that if user writes column and row with O
+int check2() {
    cout << "Enter column: ";
     cin >> col;
     cout << "Enter row: ";
@@ -132,605 +135,607 @@ int check2() { // the function that checks that if user writes column and row wi
 
 
 int main() {
-    draw(); // draws
-    check(); // checks
-	// IT ifs TIME!
+    draw();
+    cout << "Type X name: ";
+    cin >> name;
+    cout << "Type O name: ";
+    cin >> name2;
+    check();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
  if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
   if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
  if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
     check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
 
     if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     
  if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Win!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
 
     check();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 
  if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
  if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
   if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
  
 check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+     cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
 
 check();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
 
 check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
    if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
 
 check();
     if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "O" && o5 == "O" && o6 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
  }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
   if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
 
 check2();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
    if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
    if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 +" Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
 
 check();
     if (o == "X" && o2 == "X" && o3 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o2 == "O" && o3 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o4 == "O" && o5 == "O" && o6 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o4 == "X" && o5 == "X" && o6 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o7 == "O" && o8 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o7 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
    if (o == "X" && o4 == "X" && o7 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
      if (o == "O" && o4 == "O" && o7 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
     if (o2 == "O" && o5 == "O" && o8 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
     }
  if (o2 == "X" && o5 == "X" && o8 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
     }
     if (o3 == "O" && o6 == "O" && o9 == "O") {
-        cout << "You Win!\n";
+        cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o8 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  }
 if (o == "X" && o5 == "X" && o9 == "X") {
-	   cout << "You Win!\n";
+	   cout << name + " Wins!\n";
 	   exit(0);
  } 
  if (o == "O" && o5 == "O" && o9 == "O") {
-	   cout << "You Win!\n";
+	   cout << name2 + " Wins!\n";
 	   exit(0);
  }
  if (o3 == "X" && o5 == "X" && o7 == "X") {
-	cout << "You Win!\n";
+	cout << name + " Wins!\n";
 	exit(0);
  }
  if (o3 == "O" && o5 == "O" && o7 == "O") {
-	cout << "You Win!\n";
+	cout << name2 + " Wins!\n";
 	exit(0);
  }
- // finally
-// the IF to check if user has a draw.
+
  if (o == "X" or o == "O" && o2 == "X" or o2 == "O" && o3 == "X" or o3 == "O" && o4 == "X" or o4 == "O" && o5 == "X" or o5 == "O" && o6 == "X" or o6 == "O" && o7 == "X" or o7 == "O" && o8 == "X" or o8 == "O" && o9 == "X" or o9 == "O") {
     cout << "Draw!\n";
  }
